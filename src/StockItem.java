ec0e4db1327fddfdb1c86a7eb1b33d39239e6eaa
Gabor <orbangabeszb@gmail.com>
2019-05-29 20:03:36 +0200

    StockItem class setters adjusted

--- src/StockItem.java
+++ src/StockItem.java
@@ -1,4 +1,39 @@
 
-public class StockItem {
+public class StockItem implements Comparable<StockItem> {
+	private final String name;
+	private double price;
+	private int quantityStock = 0; //can be initialized later
+	
+	public StockItem(String name, double price) {
+		this.name = name;
+		this.price = price;
+		this.quantityStock = 0;
+	}
 
+	public String getName() {
+		return name;
+	}
+
+	public double getPrice() {
+		return price;
+	}
+
+	public int getQuantityStock() {
+		return quantityStock;
+	}
+
+	public void setPrice(double price) {
+		if(price > 0.0) {
+			this.price = price;
+		}	
+	}
+
+	public void adjustStock(int quantity) {
+		int newQuantity = this.quantityStock + quantity;
+		if(quantity >= 0) {
+			this.quantityStock = newQuantity;
+		}
+	}
+	
+	
 }
