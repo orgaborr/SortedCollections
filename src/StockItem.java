665af3f673f36d4ae7c42618c0d2ee0fb5ef8e0d
Gabor <orbangabeszb@gmail.com>
2019-05-31 23:47:33 +0200

    Override methods added(equals, hashcode, toString, compareTo)

--- src/StockItem.java
+++ src/StockItem.java
@@ -34,6 +34,47 @@ public class StockItem implements Comparable<StockItem> {
 			this.quantityStock = newQuantity;
 		}
 	}
+
+	@Override
+	public boolean equals(Object obj) {
+		System.out.println("Entering StockItem.equals");
+		if(obj == this) {
+			return true;
+		}
+		
+		if((obj == null) || (obj.getClass() != this.getClass())) {
+			return false;
+		}
+		
+		String objName = ((StockItem) obj).getName();
+		return this.name.equals(objName);
+	}
+
+	@Override
+	public int hashCode() {
+		return this.name.hashCode() + 31;
+	}
+
+	@Override
+	public int compareTo(StockItem o) {
+		System.out.println("Entering StockItem.compareTo");
+		if(this == o) {
+			return 0;
+		}
+		
+		if(o != null) {
+			return this.name.compareTo(o.getName());
+		}
+		
+		throw new NullPointerException();
+	}
+
+	@Override
+	public String toString() {
+		return this.name + " : price " + this.price;
+	}
+	
+	
 	
 	
 }
