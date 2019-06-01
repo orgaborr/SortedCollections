ea4c1e4a79eda254947ac1be98e5e8799de50eaf
Gabor <orbangabeszb@gmail.com>
2019-06-01 10:48:19 +0200

    StockList class and methods created

---
+++ src/StockList.java
@@ -0,0 +1,65 @@
+import java.util.Collections;
+import java.util.HashMap;
+import java.util.Map;
+
+public class StockList {
+	private final Map<String, StockItem> list;
+
+	public StockList() {
+		this.list = new HashMap<>();
+	}
+	
+	public int addStock(StockItem item) {
+		if(item  != null) {
+			//check if we already have quantities of this item
+			StockItem inStock = list.getOrDefault(item.getName(), item); //uses the item with the name,
+																		 //or uses the item passed to the method
+			//if there are already stocks of this item, adjust the quantity
+			if(inStock != item) {
+				item.adjustStock(inStock.quantityInStock());
+			}
+			
+			list.put(item.getName(), item);
+			return item.quantityInStock();
+		}
+		return 0;
+	}
+	
+	public int sellStock(String item, int quantity) {
+		StockItem inStock = list.getOrDefault(item, null); //key, defaultValue
+		
+		if((inStock != null) && (inStock.quantityInStock() >= quantity ) && (quantity > 0)) { //checks there are enough items
+			inStock.adjustStock(-quantity);
+			return quantity;
+		}
+		return 0;
+	}
+	
+	public StockItem get(String key) {
+		return this.get(key);
+	}
+	
+	public Map<String, StockItem> Items() {
+		return Collections.unmodifiableMap(list);
+	}
+
+	@Override
+	public String toString() {
+		String s = "\nStock List\n";
+		double totalCost = 0.0;
+		for(Map.Entry<String, StockItem> item : list.entrySet()) {
+			StockItem stockItem = item.getValue();
+			
+			double itemValue = stockItem.getPrice() * stockItem.quantityInStock();
+			
+			s = s + stockItem + ". There are " + stockItem.quantityInStock() + " in stock. Value of items: ";
+			s = s + itemValue + "\n";
+			totalCost += itemValue;
+		}
+		
+		return s + "Total stock value " + totalCost;
+	}
+	
+	
+	
+}
